85c17d817de5ef19f5b3f7c9a45a4a1cb43925e2
Susanto <nerdyspook007@gmail.com>
2022-05-01 10:15:50 +0530

    refactor: added links in sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import {
-    MdSubscriptions,
     MdExitToApp,
     MdThumbUp,
     MdHistory,
@@ -9,7 +8,7 @@ import {
 } from "react-icons/md";
 import { AiOutlineClockCircle } from "react-icons/ai";
 import "./Sidebar.scss";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Sidebar = ({ show, setShowNav }) => {
     const { pathname } = useLocation();
@@ -19,43 +18,43 @@ const Sidebar = ({ show, setShowNav }) => {
             className={`video__sidebar ${show && "open"} ${
                 pathname === "/watch" && "hide"
             }`}
-            onClick={() => setShowNav((prevValue) => !prevValue)}
         >
-            <li>
+            <Link to="/" onClick={() => setShowNav((prevValue) => !prevValue)}>
                 <MdHome size={23} />
                 <span>Home</span>
-            </li>
+            </Link>
 
-            <li>
-                <MdSubscriptions size={23} />
-                <span>Subscriptions</span>
-            </li>
-
-            <li>
+            <Link to="/" onClick={() => setShowNav((prevValue) => !prevValue)}>
                 <MdThumbUp size={23} />
                 <span>Liked Videos</span>
-            </li>
+            </Link>
 
-            <li>
+            <Link to="/" onClick={() => setShowNav((prevValue) => !prevValue)}>
                 <MdHistory size={23} />
                 <span>History</span>
-            </li>
+            </Link>
 
-            <li>
+            <Link to="/" onClick={() => setShowNav((prevValue) => !prevValue)}>
+                {" "}
                 <MdLibraryBooks size={23} />
                 <span>Library</span>
-            </li>
+            </Link>
 
-            <li>
+            <Link to="/" onClick={() => setShowNav((prevValue) => !prevValue)}>
                 <AiOutlineClockCircle size={23} />
                 <span>Watch Later</span>
-            </li>
+            </Link>
 
             <hr />
-            <li>
+
+            <Link
+                to="/auth"
+                onClick={() => setShowNav((prevValue) => !prevValue)}
+            >
                 <MdExitToApp size={23} />
                 <span>Log Out</span>
-            </li>
+            </Link>
+
             <hr />
         </nav>
     );
