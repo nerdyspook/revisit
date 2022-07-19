0a2d4e3e7ebc759aceaedd670bf232f90e1e99ba
Susanto <nerdyspook007@gmail.com>
2022-07-20 01:35:10 +0530

    feat: added readme

--- src/utilities/Auth/login.js
+++ src/utilities/Auth/login.js
@@ -1,31 +1,31 @@
 import axios from "axios";
 
 export const loginUser = async (
-    email,
-    password,
-    dispatchAuth,
-    navigate,
-    location
+  email,
+  password,
+  dispatchAuth,
+  navigate,
+  location
 ) => {
-    try {
-        const response = await axios({
-            method: "POST",
-            url: "/api/auth/login",
-            data: { email, password },
-        });
+  try {
+    const response = await axios({
+      method: "POST",
+      url: "/api/auth/login",
+      data: { email, password },
+    });
 
-        if (response.status === 200) {
-            dispatchAuth({
-                type: "GET_USER_DETAILS",
-                payload: response.data.foundUser,
-            });
-            localStorage.setItem("token", response.data.encodedToken);
-            localStorage.setItem("name", response.data.foundUser.firstName);
+    if (response.status === 200) {
+      dispatchAuth({
+        type: "GET_USER_DETAILS",
+        payload: response.data.foundUser,
+      });
+      localStorage.setItem("token", response.data.encodedToken);
+      localStorage.setItem("name", response.data.foundUser.firstName);
 
-            const redirectPath = location?.state?.path || "/";
-            navigate(redirectPath, { replace: true });
-        }
-    } catch (e) {
-        console.log("Error:", email, password, e);
+      const redirectPath = location?.state?.path || "/";
+      navigate(redirectPath, { replace: true });
     }
+  } catch (e) {
+    console.log("Error:", email, password, e);
+  }
 };
